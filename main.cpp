713fc1dcca6dbe7b91d9e1c18ad52306f95e2e4a
tc-imba <liuyh615@sjtu.edu.cn>
2017-01-25 23:34:36 +0800

    qt

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "data.h"
+#include <QString>
 
 using namespace std;
 
-int main() {
+int main()
+{
     data defaultData;
     defaultData.searchpeople("祝羽弦");
-
+    QString str = "祝羽弦";
+    std::cout << str.toStdString();
 
     return 0;
 }
